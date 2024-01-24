f37c05ace35db98e43afbb7d4ade0af957b3b1db
Amitrisedubey <amitrisedubey@gmail.com>
2024-01-24 14:44:19 +0530

    180 degree

--- Level 2/2.Time & Space Complexity/3.Rotate By 180.js
+++ Level 2/2.Time & Space Complexity/3.Rotate By 180.js
@@ -1,6 +1,10 @@
 /*
 You are given a square matrix if size NxN Rotate the matrix
 by 180 degree.
+output =[
+  [12,11,10,9],
+  [8,7,6,5],
+  [4,3,2,1]]
 */
 let N = 3;
 let Matrix = [
@@ -9,6 +13,19 @@ let Matrix = [
   [9, 10, 11, 12],
 ];
 console.log(rotateBy180(Matrix, N));
-function rotateBy180(Matrix, size) {
+function rotateBy180(Matrix, N) {
   //Write code here
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N - 1; j++) {
+      let temp = Matrix[i][j];
+      Matrix[i][j] = Matrix[i][N - j];
+      Matrix[i][N - j] = temp;
+    }
+  }
+  for (let i = 0; i < N - 1; i++) {
+    let temp = Matrix[i];
+    Matrix[i] = Matrix[N - i - 1];
+    Matrix[N - i] = temp;
+  }
+  return Matrix;
 }
